37505f2b01910a9f4fa262f183a3634918fce5e1
Sandwich <299465+dskvr@users.noreply.github.com>
2025-03-11 12:41:27 +0000

    add missing file

--- totem/python/managers/nvme_manager.py
+++
@@ -1,4 +0,0 @@
-#TODO: Determine whether to have generic storage class or specific to NVMe. Reasoning here is that 
-#different storage devices will have vastly different implementations.
-#The manager could discriminate again storage types to determine which type to load.
-#Weight pros and cons.
