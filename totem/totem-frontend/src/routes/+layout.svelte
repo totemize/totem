54a2d4376b3a4e14aa1639f92226889735680581
totem <totem@example.com>
2025-03-14 14:26:50 +0000

    comment out rerouting logico

--- totem/totem-frontend/src/routes/+layout.svelte
+++ totem/totem-frontend/src/routes/+layout.svelte
@@ -7,26 +7,26 @@
 	import { currentStage, profiles } from '../lib/stores/stage';
 	let { children } = $props();
 
-	let activeStage: Writable<string> = writable('egg');
+	// let activeStage: Writable<string> = writable('egg');
 	
 	onMount(async () => {
-		goto(`/${$currentStage}`);
+		// goto(`/${$currentStage}`);
 		await connectToRelay().catch((err) => {
 			console.error('Connection error in mount:', err);
 		});
-		for(const event of eventCache) {
-			if(event.kind === 0) {
-				profiles.update((prev) => {
-					prev.add(event.pubkey);
-					return prev;
-				});
-			}
-		}
-		currentStage.subscribe((stage) => {
-			if($activeStage === stage) return 
-			goto(`/${stage}`);
-			activeStage.set(stage as string);
-		});
+		// for(const event of eventCache) {
+		// 	if(event.kind === 0) {
+		// 		profiles.update((prev) => {
+		// 			prev.add(event.pubkey);
+		// 			return prev;
+		// 		});
+		// 	}
+		// }
+		// currentStage.subscribe((stage) => {
+		// 	if($activeStage === stage) return 
+		// 	goto(`/${stage}`);
+		// 	activeStage.set(stage as string);
+		// });
 	});
 	
 </script>
